f775f8dc080b4e192606b1724edfb3453ca36e7c
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-04-25 16:03:26 +0300

    Update RegisterController.java

--- src/main/java/com/example/demo/RegisterController/RegisterController.java
+++ src/main/java/com/example/demo/RegisterController/RegisterController.java
@@ -1,4 +1,32 @@
 package com.example.demo.RegisterController;
+/*
+ *  User registration business layer.
+ */
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//* Annotations
+@RestController //  For making restful web services.
+@RequestMapping(path = "api/v1.0/registration") // Maps web requests onto specific handler classes and/or handler methods
+@AllArgsConstructor // Auto generate an all argument constructor.
 
 public class RegisterController {
+    RegisterService registerService;
+
+    /*
+     * Constructor or use lombock.
+     */
+
+    /**
+     * User register endpoint.
+     */
+    public String register(
+            @RequestBody
+                    RegisterRequest registerRequest // The structure of the request.
+    ){
+        return registerService.register(registerRequest);
+    }
 }
