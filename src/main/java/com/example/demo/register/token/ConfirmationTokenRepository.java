d5f57f529ef591a1dc01833eb28efe29b4e987ce
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-04-26 17:51:54 +0300

    Update ConfirmationTokenRepository.java

--- src/main/java/com/example/demo/register/token/ConfirmationTokenRepository.java
+++ src/main/java/com/example/demo/register/token/ConfirmationTokenRepository.java
@@ -1,4 +1,15 @@
 package com.example.demo.register.token;
 
-public interface ConfirmationTokenRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ConfirmationTokenRepository
+        extends JpaRepository<ConfirmationToken, Long>
+{
+    Optional<ConfirmationToken> findByToken(String token);
+
+    // Or Query.
 }
