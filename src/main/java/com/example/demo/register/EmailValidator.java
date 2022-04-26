58602b70e183bffe2958affea0122542a99ad822
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-04-26 14:57:46 +0300

    Update EmailValidator.java

--- src/main/java/com/example/demo/register/EmailValidator.java
+++ src/main/java/com/example/demo/register/EmailValidator.java
@@ -1,4 +1,15 @@
 package com.example.demo.register;
 
-public class EmailValidator {
+import com.sun.xml.bind.v2.TODO;
+
+import java.util.function.Predicate;
+
+public class EmailValidator
+        implements Predicate<String>
+{
+    @Override
+    public boolean test(String s) {
+        // TODO: 26-Apr-22 Implement regex valid email check.
+        return true;
+    }
 }
