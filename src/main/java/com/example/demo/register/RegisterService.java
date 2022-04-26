ff027e89a532d3e11d2f3c4e70ca397c0946fcf6
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-04-26 15:33:41 +0300

    Update RegisterService.java

--- src/main/java/com/example/demo/register/RegisterService.java
+++ src/main/java/com/example/demo/register/RegisterService.java
@@ -1,10 +1,35 @@
 package com.example.demo.register;
 
+import com.example.demo.appuser.AppUser;
+import com.example.demo.appuser.UserRole;
+import com.example.demo.appuser.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegisterService {
+    private final UserService userService;
+    /*
+     * For email validation.
+     */
+    private final EmailValidator emailValidator;
+
     public String register(RegisterRequest registerRequest) {
-        return "it works";
+        boolean isEmailValid = emailValidator.test(registerRequest.getEmail());
+
+        if (!isEmailValid) {
+            throw new IllegalStateException("email is not valid");
+        }
+
+        return userService.registerUser(
+                new AppUser(
+                        registerRequest.getFirstName(),
+                        registerRequest.getLastName(),
+                        registerRequest.getEmail(),
+                        registerRequest.getPassword(),
+                        UserRole.USER
+                )
+        );
     }
 }
