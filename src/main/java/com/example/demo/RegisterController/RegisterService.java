8275a947d9a3586f750c113949f65ef5e9d3c574
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-04-26 14:05:57 +0300

    Update RegisterService.java

--- src/main/java/com/example/demo/RegisterController/RegisterService.java
+++ src/main/java/com/example/demo/RegisterController/RegisterService.java
@@ -1,5 +1,8 @@
 package com.example.demo.RegisterController;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegisterService {
     public String register(RegisterRequest registerRequest) {
         return "works";
