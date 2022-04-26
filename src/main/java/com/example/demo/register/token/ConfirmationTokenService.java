46868104371f91db62efce0e4a2ec664543b2f71
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-04-26 17:51:50 +0300

    Update ConfirmationTokenService.java

--- src/main/java/com/example/demo/register/token/ConfirmationTokenService.java
+++ src/main/java/com/example/demo/register/token/ConfirmationTokenService.java
@@ -1,4 +1,19 @@
 package com.example.demo.register.token;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class ConfirmationTokenService {
+
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+
+    /**
+     * Save token
+     * @param token
+     */
+    public void saveConfirmationToken(ConfirmationToken token){
+        confirmationTokenRepository.save(token);
+    }
 }
