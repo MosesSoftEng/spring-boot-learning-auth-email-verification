688be01469b864b4ba7816b59c5863b84e6e7f79
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-04-26 15:49:07 +0300

    Update ConfirmationToken.java

--- src/main/java/com/example/demo/register/token/ConfirmationToken.java
+++ src/main/java/com/example/demo/register/token/ConfirmationToken.java
@@ -1,4 +1,67 @@
 package com.example.demo.register.token;
 
+import com.example.demo.appuser.AppUser;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/*
+ * Token Model
+ */
+@Getter
+@Setter
+@NoArgsConstructor
 public class ConfirmationToken {
+    @Id //* JPA Annotation for marking a primary key in this case property id.
+
+    //* JPA for autoincrement.
+    @SequenceGenerator(
+            name = "confirmation_token_sequence",
+            sequenceName = "confirmation_token_sequence",
+            allocationSize = 1
+    )
+
+    // JPA annotation for generating value.
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "confirmation_token_sequence"
+    )
+    private Long id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    private LocalDateTime confirmedAt;
+
+    //* One user many confirmation tokens.
+    @ManyToOne
+    @JoinColumn (
+            nullable = false,
+            name = "app+user_id"
+    )
+    private AppUser appUser;
+
+    /*
+     * Constructor
+     */
+    public ConfirmationToken(String token,
+                             LocalDateTime createdAt,
+                             LocalDateTime expiresAt,
+                             LocalDateTime confirmedAt,
+                             AppUser appUser) {
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiresAt = expiresAt;
+        this.confirmedAt = confirmedAt;
+        this.appUser = appUser;
+    }
 }
