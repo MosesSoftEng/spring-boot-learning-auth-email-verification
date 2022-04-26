a97a39899c824686268e7ed8c795fe2ad74252db
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-04-26 14:06:10 +0300

    Create UserService.java

---
+++ src/main/java/com/example/demo/appuser/UserService.java
@@ -0,0 +1,33 @@
+package com.example.demo.appuser;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/*
+ * Service to manage login/ register.
+ */
+@Service // Used with classes that provide some business functionalities.
+
+// * Lombok annotations
+@AllArgsConstructor // Auto generate an all argument constructor.
+public class UserService implements
+        UserDetailsService // Add Spring security service
+{
+    private final UserRepository userRepository;
+
+    /*
+     * Constructor or use lombok
+     */
+
+    //! Username is email in this case.
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        String.format("No user with email %s.", email)
+                ));
+    }
+}
