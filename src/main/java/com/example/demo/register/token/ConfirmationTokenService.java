2cfa86259e2b6d9a6002b42fbc511e01e3da3d47
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-04-28 20:56:59 +0300

    Update ConfirmationTokenService.java

--- src/main/java/com/example/demo/register/token/ConfirmationTokenService.java
+++ src/main/java/com/example/demo/register/token/ConfirmationTokenService.java
@@ -3,6 +3,9 @@ package com.example.demo.register.token;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class ConfirmationTokenService {
@@ -15,5 +18,16 @@ public class ConfirmationTokenService {
      */
     public void saveConfirmationToken(ConfirmationToken token){
         confirmationTokenRepository.save(token);
+    }
+
+    public Optional<ConfirmationToken> getToken(String token) {
+        return confirmationTokenRepository.findByToken(token);
+    }
+
+    public int setConfirmedAt(String token) {
+        return confirmationTokenRepository.updateConfirmedAt(
+                token,
+                LocalDateTime.now()
+        );
     }
 }
